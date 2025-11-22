3204f24eec07fe064c1e6fbb4379e1e4cd88e24f
enzosas <enzo.silveira2@gmail.com>
2025-11-22 16:34:06 -0300

    remocao de acentos

--- t2/irq.c
+++ t2/irq.c
@@ -9,7 +9,7 @@ static char *nomes[N_IRQ] = {
   [IRQ_RESET] =   "Reset",
   [IRQ_ERR_CPU] = "Erro de execução",
   [IRQ_SISTEMA] = "Chamada de sistema",
-  [IRQ_RELOGIO] = "E/S: relógio",
+  [IRQ_RELOGIO] = "E/S: relogio",
   [IRQ_TECLADO] = "E/S: teclado",
   [IRQ_TELA]    = "E/S: console",
 };
